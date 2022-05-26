6c1273398f99815c08c9b23574d0cacc0494200e
Silvestre S. Tomas, jr <robert.tomas@rocketmail.com>
2022-05-26 21:01:33 +0300

    photo revision

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
     const buttons = document.querySelectorAll('.btn');
-    const imagesContent = document.querySelectorAll('.image-content');
+    const imagesContent = document.querySelectorAll('.portfolio');
 
     buttons.forEach((button) => {
         button.addEventListener('click',(ev) => {
@@ -21,30 +21,23 @@
         })
     })
 
-
-    /* const buttons = document.querySelectorAll('.btn');
-    const imagesContent = document.querySelectorAll('.image-content');
-
-    buttons.forEach((button) => {
-        button.addEventListener('click',(ev) => {
-            ev.preventDefault();
-            const filter = ev.target.dataset.filter
-
-            imagesContent.forEach((item) => {
-                if (filter === 'all'){
-                    item.style.display = 'block';
-                } else {
-                    if (item.classList.contains(filter)) {
-                        item.style.display = 'block';
-                    }else{
-                        item.style.display = 'none';
-                    }
-                }
-            })
-        })
-    })
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
 
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    navMenu.classList.toggle('open');
+  
+   
+} )
 
-
- */
+const navLink = document.querySelectorAll(".nav-link");
+ navLink.forEach((item) => {
+     item.addEventListener("click", () => {
+       if (hamburger.classList.contains("open")) {
+         hamburger.classList.toggle("open");
+         navMenu.classList.toggle("open");
+       }
+     });
+   });
